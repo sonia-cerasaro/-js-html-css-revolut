a2de95383dfdd9918cfba945995564968ea8e46e
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-03-14 16:22:53 +0100

    dropdown js funzionante

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
 $(document).ready(function(){
-  var iconDrop = $('a.my-dropdown-toggle');
 
-  iconDrop.click(function(){
-    $(this).next().toggleClass('aktive');
-  })
-
+  $(".dropdown:has(.dropdown-toggle)").click(function(){
+    if ($(this).find(".dropdown-menu").is(":visible")) {
+      $(".dropdown-menu").hide();
+    } else {
+      $(".dropdown-menu").hide();
+      $(this).find(".dropdown-menu").toggle();
+    }
+  });
 });
+
+// Dovrebbe far chiudere le finestre al click
+
+// $(document).on("click", function(event){
+//  var hide = $(".dropdown-toggle");
+//  if(hide !== event.target && !hide.has(event.target).length){
+//      $(".dropdown-menu").hide();
+//  });
+
+
+
+
+// Soluzione -non completa- con hover
+
+// $(document).ready(function() {
+//   var iconDrop = $('a.my-dropdown-toggle');
+//
+//   iconDrop.hover(function(){
+//        $(this).next().delay(20).slideDown(300);
+//   }, function(){
+//        $(this).next().delay(20).slideUp(300);
+//   });
+//
+// });
